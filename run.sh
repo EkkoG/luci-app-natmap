1a59242b14f2719f224e9c3626db672e1b134a3f
EkkoG <beijiu572@gmail.com>
2023-09-24 20:33:37 +0800

    Fixed ci

--- run.sh
+++ run.sh
@@ -2,7 +2,5 @@
 mkdir -p bin
 # macOS no need to change the owner
 # change the owner of bin to 1000:1000 when running on linux
-if [[ $(uname) =~ "Linux" ]]; then
-    sudo chown -R 1000:1000 bin
-fi
+sudo chown -R 1000:1000 bin
 docker-compose up
